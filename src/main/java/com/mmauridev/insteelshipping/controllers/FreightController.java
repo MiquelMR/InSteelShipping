a2e2229cfed50423d0ff7f5eb858b6d372f7b4b7
MiquelMR <mmauridev@gmail.com>
2025-08-18 10:08:27 +0200

    Html added

--- src/main/java/com/mmauridev/insteelshipping/controllers/FreightController.java
+++ src/main/java/com/mmauridev/insteelshipping/controllers/FreightController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/freights")
+@RequestMapping("/api/freights")
 public class FreightController {
 
     private final FreightService freightService;
